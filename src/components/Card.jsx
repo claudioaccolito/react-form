57d7ba97e1cfbd78c697a4ae0250a38325f29188
Claudio Accolito <cla>
2022-06-27 18:45:30 +0200

    page with cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+  // colors #B2EDCB #B3DFED #EECCB4
+  return (
+    <div className="card" style={{ background: "#B2EDCB" }}>
+      <div className="card-title">Step 1</div>
+      <div>card content</div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        Seminar <span style={{ color: "#4698D3" }}>Registration</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
